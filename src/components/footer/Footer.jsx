011054616008bedbf5dbed115acbcf7604989c24
ASWIN-P-INFOPRISM <aswinpalapetty@gmail.com>
2023-06-18 23:39:38 +0530

    contact: mailto an email id added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,13 +4,18 @@ import './Footer.css'
 
 function Footer() {
     const navigate = useNavigate()
+
+    const redirectToGmail = ()=>{
+      console.log("mailto:aswinpalapetty@gmail.com");
+      window.location.href = "mailto:aswinpalapetty@gmail.com";
+    }
   return (
     <div className='footer'>
         <small className='options' onClick={()=>navigate('/')}>Home</small>
         <small className='options' onClick={()=>navigate('/viewAll/originals')}>Originals</small>
         <small className='options' onClick={()=>navigate('/viewAll/trending')}>Trending</small>
         <small className='options' onClick={()=>navigate('/viewAll/popular')}>Popular</small>
-        <small className='options' onClick={()=>navigate('/contact')}>Contact us</small><br/>
+        <small className='options' onClick={()=>redirectToGmail()}>Contact us</small><br/>
         <small className='copyright'>© 2022 Copyright: </small><small>All rights reserved. Designed by </small>
         <a href='https://aswin-p-infoprism.github.io/My-Webpage/index.html'><small className='developer'>ASWIN</small></a>
     </div>
